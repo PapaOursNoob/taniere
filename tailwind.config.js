5210aeb7c5b88ebc9747420d9dfb617290b8132d
PapaOursNoob <metalgwen@gmail.com>
2024-11-14 12:23:35 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#660099',
+        secondary: '#FFBB00',
+      },
+    },
+  },
+  plugins: [],
+}
